d5eeb2b80096e07a189d609de03359645a684077
evelyn <Evelyn-stoll@gmx.de>
2020-10-30 08:43:54 +0100

    - Bootstrap Icons per npm install hinzugefügt - neue Styling anpassung durchgeführt - Neue icons eingebaut

--- public/js/ProfilScript.js
+++ public/js/ProfilScript.js
@@ -59,7 +59,10 @@ Vorname.addEventListener("submit", (evt)=> {
     if (bearbeitbarV == false){
         document.querySelector('#Vorname').readOnly = false;
         bearbeitbarV = true;
-        document.querySelector('#vnameb').innerHTML = '<i class="glyphicon glyphicon-ok" style="font-size: 20px;"></i>';
+        document.querySelector('#vnameb').innerHTML = '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-check-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">' +
+        '<path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>' +
+        '<path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>'
+        + '</svg>';
         document.querySelector('#Vorname').style.backgroundColor = "rgba(255, 255, 255, 0.644)";
     }
     else {
@@ -67,7 +70,10 @@ Vorname.addEventListener("submit", (evt)=> {
         console.log(values);
         document.querySelector('#Vorname').readOnly = true;
         bearbeitbarV = false;
-        document.querySelector('#vnameb').innerHTML = '<i class="glyphicon glyphicon-pencil" style="font-size: 20px;"></i>';
+        document.querySelector('#vnameb').innerHTML = '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">' +
+        '<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>' +
+        '<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>' +
+        '</svg>';
         document.querySelector('#Vorname').style.background = "none";
         fetch("/vornameaktualisierung", {
             method: "PATCH",
@@ -90,7 +96,10 @@ Nachname.addEventListener("submit", (evt)=> {
     if (bearbeitbarN == false){
         document.querySelector('#Nachname').readOnly = false;
         bearbeitbarN = true;
-        document.querySelector('#nnameb').innerHTML = '<i class="glyphicon glyphicon-ok" style="font-size: 20px;"></i>';
+        document.querySelector('#nnameb').innerHTML = '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-check-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">' +
+        '<path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>' +
+        '<path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>'
+        + '</svg>';
         document.querySelector('#Nachname').style.backgroundColor = "rgba(255, 255, 255, 0.644)";
     }
     else {
@@ -98,7 +107,10 @@ Nachname.addEventListener("submit", (evt)=> {
         console.log(values);
         document.querySelector('#Nachname').readOnly = true;
         bearbeitbarN = false;
-        document.querySelector('#nnameb').innerHTML = '<i class="glyphicon glyphicon-pencil" style="font-size: 20px;"></i>';
+        document.querySelector('#nnameb').innerHTML = '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">' +
+        '<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>' +
+        '<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>' +
+        '</svg>';
         document.querySelector('#Nachname').style.background = "none";
         fetch("/nachnameaktualisierung", {
             method: "PATCH",
@@ -121,7 +133,10 @@ Email.addEventListener("submit", (evt)=> {
     if (bearbeitbarE == false){
         document.querySelector('#Email').readOnly = false;
         bearbeitbarE = true;	
-      document.querySelector('#emailb').innerHTML = '<i class="glyphicon glyphicon-ok" style="font-size: 20px;"></i>';
+      document.querySelector('#emailb').innerHTML = '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-check-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">' +
+      '<path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>' +
+      '<path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>'
+      + '</svg>';
       document.querySelector('#Email').style.backgroundColor = "rgba(255, 255, 255, 0.644)";
     }
     else {
@@ -129,7 +144,10 @@ Email.addEventListener("submit", (evt)=> {
         console.log(values);
         document.querySelector('#Email').readOnly = true;
         bearbeitbarE = false;
-        document.querySelector('#emailb').innerHTML = '<i class="glyphicon glyphicon-pencil" style="font-size: 20px;"></i>';
+        document.querySelector('#emailb').innerHTML = '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">' +
+        '<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>' +
+        '<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>' +
+        '</svg>';
         document.querySelector('#Email').style.background = "none";
         fetch("/emailaktualisierung", {
             method: "PATCH",
@@ -152,7 +170,10 @@ Telefon.addEventListener("submit", (evt)=> {
     if (bearbeitbarT == false){
         document.querySelector('#Telefon').readOnly = false;
         bearbeitbarT = true;
-        document.querySelector('#telb').innerHTML = '<i class="glyphicon glyphicon-ok" style="font-size: 20px;"></i>';
+        document.querySelector('#telb').innerHTML = '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-check-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">' +
+        '<path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>' +
+        '<path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>'
+        + '</svg>';
         document.querySelector('#Telefon').style.backgroundColor = "rgba(255, 255, 255, 0.644)";
     }
     else {
@@ -160,7 +181,10 @@ Telefon.addEventListener("submit", (evt)=> {
         console.log(values);
         document.querySelector('#Telefon').readOnly = true;
         bearbeitbarT = false;
-        document.querySelector('#telb').innerHTML = '<i class="glyphicon glyphicon-pencil" style="font-size: 20px;"></i>';
+        document.querySelector('#telb').innerHTML = '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">' +
+        '<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>' +
+        '<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>' +
+        '</svg>';
         document.querySelector('#Telefon').style.background = "none";
         fetch("/telefonaktualisierung", {
             method: "PATCH",
